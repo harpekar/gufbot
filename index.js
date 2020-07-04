4b1973864949590e446e2c2b83acd92ecced60d6
Karen <karenl.harper88@gmail.com>
2020-07-04 14:21:05 -0700

    First Commit

---
+++ index.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const TOKEN = process.env.TOKEN;
+
+bot.login(TOKEN);
+
+var tap_dead = 0;
+
+bot.on('ready', () => {
+  console.info(`Logged in as ${bot.user.tag}!`);
+});
+
+bot.on('message', msg => {
+  if (msg.content === 'where is tap?') {
+    msg.channel.send('dead to me');
+    }
+
+    else if (msg.content.includes("tap" && "dead")) {
+    tap_dead++;
+
+    msg.channel.send(`Tap is dead to me ${tap_dead} times over.`)     
+    }  
+
+
+});
